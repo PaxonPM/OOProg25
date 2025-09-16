dc041c4ff896084c48f28034a5a63cbb5024ed86
Paw Møller <pawmoeller94@gmail.com>
2025-09-16 11:44:07 +0200

    Add LegoGame project with basic Lego class and demo
    Introduces a new LegoGame .NET project containing a Lego class with movement and orientation logic, and a Program.cs that demonstrates its usage. Includes project and solution files for setup.

---
+++ Ekstra/LegoGame/Lego.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoGame
+{
+    public class Lego
+    {
+
+        //felter
+        private string _color;
+        private int _legoSize;
+        private int _xPos;
+        private int _yPos;
+        private bool _orientationHorizontal;
+
+
+        public string Color { get { return _color; } }
+
+        public int XPos { get { return _xPos; } }
+        public int YPos { get { return _yPos; } }
+        public bool OrientaionHorizontal { get { return _orientationHorizontal; } set { _orientationHorizontal = value; } }
+
+
+        public Lego(string color, int legoSize, int xPos, int yPos, bool legoHorizontal)
+        {
+            _color = color;
+            _legoSize = legoSize;
+            _xPos = xPos;
+            _yPos = yPos;
+            _orientationHorizontal = legoHorizontal;
+
+        }
+
+        public void Up(int move)
+        {
+            _yPos += move;
+
+            if (_yPos >=20)
+            {
+                int tempY = _yPos-20;
+                _yPos = 0;
+                _yPos += tempY;
+            }
+        }
+
+        public void Down( int move)
+        {
+            _yPos -= move;
+
+            if (_yPos <= 0)
+            {
+                int tempY = Math.Abs(_yPos);
+                _yPos = 20;
+                _yPos -= tempY;
+            }
+        
+            
+        }
+        public void left(int move)
+        {
+            _xPos -= move;
+
+            if (_xPos <= 0)
+            {
+                int tempX = Math.Abs(_xPos);
+                _xPos = 20;
+                _xPos -= tempX;
+            }
+        }
+        public void Right(int move)
+        {
+            _xPos += move;
+
+            if (_xPos >= 20)
+            {
+                int tempX = _xPos - 20;
+                _xPos = 0;
+                _xPos += tempX;
+            }
+        }
+
+    }
+}
---
+++ Ekstra/LegoGame/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using LegoGame;
+
+Lego objLego = new Lego("blue", 3, 15, 15, true);
+
+Console.WriteLine($"the current position is: {objLego.XPos},{objLego.YPos}");
+Console.WriteLine($"The legos color is {objLego.Color}");
+Console.WriteLine($"The legos is horizontal: {objLego.OrientaionHorizontal}");
+
+
+objLego.OrientaionHorizontal = false;
+objLego.Right(6);
+objLego.Up(6);
+Console.WriteLine($"\nthe current position is: {objLego.XPos},{objLego.YPos}");
+Console.WriteLine($"The legos is horizontal: {objLego.OrientaionHorizontal}");
+
+
+objLego.Right(-17);
+objLego.Up(-18);
+Console.WriteLine($"\nthe current position is: {objLego.XPos},{objLego.YPos}");
+Console.WriteLine($"The legos is horizontal: {objLego.OrientaionHorizontal}");
