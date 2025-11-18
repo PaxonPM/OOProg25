8ed8bcd8f2e206304e87f9bd20c84d35a4459025
Paw Møller <pawmoeller94@gmail.com>
2025-11-18 10:33:14 +0100

    Add Damager and Defender character types
    Introduced Damager and Defender classes with custom damage modification logic. Updated Program.cs to include these new character types in both teams for battle simulation.

---
+++ Chap/OOProg02/Unsolved/RolePlayV23/Damager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RolePlayV23
+{
+    public class Damager : Character
+    {
+
+        public Damager(string name, int hitPoints, int minDamage, int maxDamage)
+            : base(name, hitPoints, minDamage, maxDamage) { }
+
+
+        protected override int DealDamageModifyChance
+        {
+            get { return 40; }
+        }
+
+        protected override int CalculateModifiedDealDamage(int receivedDamage)
+        {
+            return receivedDamage * 2;
+        }
+    }
+}
--- Chap/OOProg02/Unsolved/RolePlayV23/Program.cs
+++ Chap/OOProg02/Unsolved/RolePlayV23/Program.cs
@@ -1,10 +1,15 @@
-﻿
+﻿using RolePlayV23;
+
 CharacterGroup redTeam = new CharacterGroup("Team Red");
 redTeam.AddCharacter(new Character("Angor", 100, 15, 25));
 redTeam.AddCharacter(new Character("Zurin", 85, 18, 30));
+redTeam.AddCharacter(new Defender("DefenderRed", 60, 12, 44));
+redTeam.AddCharacter(new Damager("DamagerRed", 76, 14, 42));
+
 
 CharacterGroup greenTeam = new CharacterGroup("Team Green");
 greenTeam.AddCharacter(new Character("Baldur", 120, 12, 18));
 greenTeam.AddCharacter(new Character("Eliza", 80, 20, 35));
-
+greenTeam.AddCharacter(new Defender("DefenderGreen", 60, 12, 44));
+greenTeam.AddCharacter(new Damager("DamagerGreen", 76, 14, 42));
 BattleHandler.DoBattle(redTeam, greenTeam);
